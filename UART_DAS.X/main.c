6bcfb20a2f782c7ec3aeeda20e3ea2fa37266484
GeorgeCh <georgechirica92@gmail.com>
2020-08-30 10:51:39 +0300

    - added the oled

--- UART_DAS.X/main.c
+++ UART_DAS.X/main.c
@@ -9,27 +9,32 @@
 #include "bit_settings.h"
 #include "config.h"
 #include "data_out.h"
+#include "oled.h"
 
 char receive_buffer;
-
 void main(void) 
 {
     IO_First_Init();
     Configure_Clock();    
+    OLED_Init(0x3C);
+
+    OLED_clear();
+    OLED_string("Hello World69!", 0, 0);
+    OLED_write();
     
     init_data_out(9600);
     init_timebase();
-    
-    while(1){
-        LATCbits.LATC1 = 0;
+        while(1){
+        LATCbits.LATC7 = 0;
         __delay_us(1);
-        LATCbits.LATC1 = 1;
+        LATCbits.LATC7 = 1;
         __delay_us(1);
     }
     
     
     
 }
+
 //interrupt routine
 void __interrupt() timer(void)
 {
